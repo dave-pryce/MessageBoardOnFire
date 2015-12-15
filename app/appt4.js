991bc16eedcc4d6c14edad2e6941c8c61c87617e
David Pryce <dpryce@seek.com.au>
2015-12-16 08:38:32 +1100

    trying to get edit to work.

--- app/appt4.js
+++ app/appt4.js
@@ -1,7 +1,9 @@
+
+// factory to get all messages in arrary
 angular.module("sampleApp").factory("chatMessages",["$firebaseArray",
   function($firebaseArray) {
     // db reference
-    var randomRoomId = Math.round(Math.random() * 100000000);
+    //var randomRoomId = Math.round(Math.random() * 100000000);
     var ref = new Firebase("https://blazing-inferno-4471.firebaseio.com/messages"); // + randomRoomId);
 
     return $firebaseArray(ref);
@@ -10,23 +12,7 @@ angular.module("sampleApp").factory("chatMessages",["$firebaseArray",
 
 
 
-//angular.module("sampleApp").controller("HistCtrl", ["$scope", "$firebaseArray",
-//function($scope, $firebaseArray) {
-//  var messagesRef = new Firebase("https://blazing-inferno-4471.firebaseio.com/messages");
-  // download data from Firebase
-//  $scope.filteredMessages = $firebaseArray(messagesRef);
-  // create a query for the most recent 25 messages on the server
-     //var query = messagesRef.orderByChild("timestamp").limitToLast(3);
-     // the $firebaseArray service properly handles database queries as well
-     //$scope.filteredMessages = $firebaseArray(query);
-//}
-//]);
-
-
-
-
-
-
+// controller to add new messages
 angular.module("sampleApp").controller("MsgCtrl", ["$scope", "chatMessages",
   function($scope, chatMessages) {
 
@@ -39,18 +25,32 @@ angular.module("sampleApp").controller("MsgCtrl", ["$scope", "chatMessages",
         content: $scope.message,
         timestamp: Firebase.ServerValue.TIMESTAMP
       });
-
+    }
       $scope.message = "";
     }
+  ]);
 
 
-    ///////////////////////////////////////// add message
-    //var messages = $firebaseArray(messagesRef);
-    // add a new record to the list
-    //messages.$add({
-    //  user: "D E",
-    //  text: "Hello world 2"
-  //  });
+/// Factory to find individual message
+angular.module('sampleApp').factory('Message',["$firebaseObject",
+  function($firebaseObject) {
+    return function(message) {
+      // create ref to db node where data is stored
+      //var randomId = Math.round(Math.random() * 100000000);
+      var ref = new Firebase("https://blazing-inferno-4471.firebaseio.com/messages"); //+ randomId);
+      var messageRef = ref.child(message);
 
+      // return it as a synchronised object
+      return $firebaseObject(messageRef);
+    }
+  }
+]);
+
+
+
+angular.module('sampleApp').controller("MsgEditCtrl",["$scope", "Message",
+  function($scope, Message){
+    // put profile in scope in DOM
+    $scope.editmessage = Message("-K5bOTuWZfsSl31pokBN").$bindTo($scope, "Message");
   }
 ]);
