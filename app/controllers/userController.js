d8a588aa953b5b8e7c3dc0e448f24fadd0bccf1b
David Pryce <dpryce@seek.com.au>
2016-01-21 08:54:37 +1100

    adding user authentication

---
+++ app/controllers/userController.js
@@ -0,0 +1,18 @@
+angular.module("sampleApp").controller("userCtrl", ["$scope", "Auth",
+  function($scope, Auth){
+    $scope.createUser = function(){
+      $scope.alert = null;
+      $scope.error = null;
+
+      Auth.$createUser({
+        email: $scope.email,
+        password: $scope.password
+      }).then(function(userData) {
+        $scope.alert = "User Created with uid: " + userData.uid;
+      }).catch(function(error) {
+        $scope.error = error;
+      });
+
+    };
+  }
+])
--- app/factories/userAuth.js
+++ app/factories/userAuth.js
@@ -1,2 +1,7 @@
 ///// Factory for User Authentication //////////////////
-//angular.moduel("sampleApp")
+angular.module("sampleApp").factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var ref = new Firebase("https://blazing-inferno-4471.firebaseio.com/messages");
+    return $firebaseAuth(ref);
+  }
+]);
