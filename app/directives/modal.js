f964588fcc51dd4212ed9954ba6fb2bc800c82e8
dave-pryce <dp@Davids-MacBook-Pro.local>
2016-05-08 13:55:42 +1000

    Getting too complex.

--- app/directives/modal.js
+++ app/directives/modal.js
@@ -1,4 +1,4 @@
-angular.module("sampleApp").controller('ModalCtrl', function ($scope, $uibModal, $log) {
+angular.module("sampleApp").controller('ModalCtrl', ["Auth", function ($scope, $uibModal, $log, Auth) {
 
   $scope.animationsEnabled = true;
 
@@ -22,7 +22,7 @@ angular.module("sampleApp").controller('ModalCtrl', function ($scope, $uibModal,
     $scope.animationsEnabled = !$scope.animationsEnabled;
   };
 
-});
+}]);
 
 // Please note that $uibModalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
@@ -30,11 +30,25 @@ angular.module("sampleApp").controller('ModalCtrl', function ($scope, $uibModal,
 angular.module('ui.bootstrap').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
 
 
-  $scope.signIn() = function () {
-    $uibModalInstance.close();
+  $scope.signIn = function () {
+    Auth.$authWithPassword({
+      email: $scope.emailIn,
+      password: $scope.passwordIn
+    }).then(function(authData) {
+      $scope.error = null;
+      $scope.emailIn = null;
+      $scope.passwordIn = null;
+      // signin to expired on browser shut down
+      remember : "sessionOnly";
+      $modalInstance.close();
+      $uibModalInstance.close();
+      /*(#signin).hide;*/
+    }).catch(function(error) {
+      $scope.signInError = "Authentication failed: " + error;
+    });
   };
 
-//  $scope.cancel = function () {
-//    $uibModalInstance.dismiss('cancel');
-//  };
+ $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+ };
 });
