ba2ae629480e295ccf78af44e4590005e392ccd9
dave-pryce <dp@Davids-MacBook-Pro.local>
2016-06-03 08:44:22 +1000

    routing added - errors with Maximum call stack size exceeded.

--- app/routes/route.js
+++ app/routes/route.js
@@ -2,16 +2,12 @@ angular.module("sampleApp").config(function($routeProvider){
   $routeProvider
 
   .when ('/', {
-    redirectTo: '/home'
-  })
-
-  .when ('/home', {
     templateUrl : "index.html"
   })
 
-//  .when ('/account', {
-//    templateUrl : xxxx
-//  })
+ .when ('/account', {
+    templateUrl : "account.html"
+  })
 
 
 })
